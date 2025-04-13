1eee63d39cacb86a48e15242950e850f6442a31d
Pericles Feitoza <periclesanfe@gmail.com>
2025-04-13 01:48:28 -0300

    primeiros exercícios com variaveis e operações

---
+++ calculadoraAreaRetangulo/CalculadoraAreaRetangulo.java
@@ -0,0 +1,6 @@
+public class CalculadoraAreaRetangulo {
+    public static void main(String[] args) {
+        double largura = 5.0, altura = 3.0;
+        System.out.println(largura * altura);
+    }
+}
---
+++ controleEstoquePadaria/controleEstoquePadaria.java
@@ -0,0 +1,8 @@
+public class controleEstoquePadaria {
+    public static void main(String[] args) {
+        int quantidadeFarinha = 10;
+        int quantidadeAcucar = 5;
+        System.out.println("Quantidade de farinha: " + quantidadeFarinha + " kg");
+        System.out.println("Quantidade de açúcar: " + quantidadeAcucar + " kg");
+    }
+}
