a5151f18583f79e1496753efbf3fd68f5bf24e34
Pericles Feitoza <periclesanfe@gmail.com>
2025-04-13 01:36:19 -0300

    mudei de curso

--- java-anatomia-classes/src/dio/javacloud/javabasico/anatomiaclasses/MinhaClasse.java
+++
@@ -1,6 +0,0 @@
-package dio.javacloud.javabasico.anatomiaclasses;
-public class MinhaClasse {
-   public static void main (String [] args) {
-    System.out.print("Olá, Mundo!");
-   } 
-}
--- java-anatomia-classes/src/dio/javacloud/javabasico/anatomiaclasses/MinhaClasse5.java
+++
@@ -1,4 +0,0 @@
-package dio.javacloud.javabasico.anatomiaclasses;
-public class MinhaClasse5 {
-    
-}
