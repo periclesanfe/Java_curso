f9b1c6c8062174095099571780b142fb4f6493ba
Pericles Feitoza <periclesanfe@gmail.com>
2025-04-14 01:10:16 -0300

    OperadoresAtribuicao

---
+++ operadoresAtribuicao/OperadoresAtribuicao.java
@@ -0,0 +1,12 @@
+package operadoresAtribuicao;
+
+public class OperadoresAtribuicao {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero+=3;
+        numero -=2;
+        numero*=4;
+        numero/=2;
+        System.out.println("O resultado é: " + numero);
+    }
+}
